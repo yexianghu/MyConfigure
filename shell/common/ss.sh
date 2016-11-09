e2ce92ac7140b8ddf3a699ce654aa637b15dc8e6
xianghuy <xianghuy@opera.com>
2016-11-09 19:26:47 +0800

    set password in cmd

--- shell/common/ss.sh
+++ shell/common/ss.sh
@@ -1,2 +1,2 @@
-alias startss="sudo sslocal -c $SHELL_PATH/data/ss.json -d start"
+alias startss="sudo sslocal -c $SHELL_PATH/data/ss.json -d start -k $SS_PW"
 alias stopss="sudo sslocal -c $SHELL_PATH/data/ss.json -d stop"
