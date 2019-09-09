4ec273e8a8f64950e025c8a25908ff86991b0687
shawn.ye <shawn@htek.com>
2019-09-09 09:34:12 +0800

    add busybox

--- shell/common/all.sh
+++ shell/common/all.sh
@@ -12,4 +12,4 @@ source ${SHELL_COMMON_PATH}/local.sh
 source ${SHELL_COMMON_PATH}/flutter.sh
 source ${SHELL_COMMON_PATH}/util.sh
 
-export PATH=$PATH:/usr/local/go/bin:~yezi/bin:/opt/genymotion:$ANDROID_SDK_DIR/tools/:$ANDROID_SDK_DIR/platform-tools/:$ANDROID_NDK_DIR
+export PATH=$PATH:/usr/local/go/bin:~yezi/bin:/opt/genymotion:$ANDROID_NDK_DIR
--- shell/common/android.sh
+++ shell/common/android.sh
@@ -4,8 +4,16 @@ export ANDROID_NDK_DIR=~/Android/Ndk
 export ANDROID_NDK_HOME=$ANDROID_NDK_DIR
 #for android build
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
-alias adb_refresh='sudo $ANDROID_SDK_DIR/platform-tools/adb kill-server && sudo $ANDROID_SDK_DIR/platform-tools/adb start-server'
+alias adb_refresh='sudo adb kill-server && sudo adb start-server'
 alias aroot='adb root && adb remount'
 alias averity='aroot && adb disable-verity && adb reboot'
 alias restart_jack_server='out/host/linux-x86/bin/jack-admin kill-server && out/host/linux-x86/bin/jack-admin start-server'
 alias sync='repo sync -c -d --force-sync --no-tags --prune'
+install_busybox() {
+    if [ ! -f ~/.cache/busybox ]; then
+        wget https://busybox.net/downloads/binaries/1.21.1/busybox-armv7l -O ~/.cache/busybox 
+    fi
+    adb push ~/.cache/busybox /system/xbin/busybox
+    adb shell chmod +x /system/xbin/busybox
+    adb shell /system/xbin/busybox --install /system/xbin/ 
+}
